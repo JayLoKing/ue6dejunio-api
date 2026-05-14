fe7cfa0e50d274e406099dbf15ce844a2ef81f0e
Pabel Ivan Mendez Chambi <157851331+JayLoKing@users.noreply.github.com>
2026-05-14 11:38:14 -0400

    Initial commit

---
+++ src/main/java/bo/edu/univalle/sis/ue6dejunio_api/Ue6dejunioApiApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.univalle.sis.ue6dejunio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ue6dejunioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ue6dejunioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/edu/univalle/sis/ue6dejunio_api/Ue6dejunioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.univalle.sis.ue6dejunio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ue6dejunioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
